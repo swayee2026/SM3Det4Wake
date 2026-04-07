0eb4d175bc1167c0ceaebb2cb511649a5a8fdb6c
swayee2023 <suiyi_liu@mail.ustc.edu.cn>
2026-04-07 22:11:04 +0800

    delete unrelated files

--- BabelRS_pretrain/eval_vg.sh
+++
@@ -1,5 +0,0 @@
-for dataset in "dior-rsvg" "geochat" "rsvg" "vrsbench"
-do
-    GPUS=8 bash evaluate.sh "models/ViTP_InternVL_1B_rs" $dataset --dynamic 
-done
-
--- BabelRS_pretrain/run.sh
+++
@@ -1,1 +0,0 @@
-GPUS=8 PER_DEVICE_BATCH_SIZE=1 sh babelrs_configs/BabelRS_internvl_1b_20ksteps.sh
